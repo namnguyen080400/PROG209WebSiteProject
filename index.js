70f99dcdd05f84a8d5453c7e2c188c4f4da03106
namnguyen080400 <namq.nguyen@bellevuecollege.edu>
2023-05-12 20:01:53 -0700

    version 1 completed

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 let contactArray = [];
 
-let ContactObject = function (pName, pEmail, pPhoneNumber) {
+let  ContactObject = function (pName, pEmail, pPhoneNumber) {
     this.name = pName;
     this.email = pEmail;
     this.phoneNumber = pPhoneNumber;
     this.ID = Math.random().toString(16).slice(5);
 }
 
-
 // code runs immediately
 //================================================================
 
-// runs  when dom is loaded
+// runs when DOM is loaded
 document.addEventListener("DOMContentLoaded", function (event) {
 
     createList();
 
     document.getElementById("submitContact").addEventListener("click", function () {
-        var contact = new ContactObject(document.getElementById("name").value,
-        document.getElementById("email").value, document.getElementById("phone").value);
-        contactArray.push ( contact );
-        
+        var contact = new ContactObject(
+            document.getElementById("name").value,
+            document.getElementById("email").value,
+            document.getElementById("phone").value
+        );
+        contactArray.push(contact);
+
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
         document.getElementById("phone").value = "";
@@ -29,25 +31,35 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.location.href = "index.html#display";
     });
 
-    $(document).on("pagebeforeshow", "#display", function (event) {   // have to use jQuery 
+    $(document).on("pagebeforeshow", "#display", function (event) {
         createList();
-    })
+    });
+//=====part where i add  =====
+    document.getElementById("police").addEventListener("click", function () {
+        alert("Police number 911");
+    });
 
+    document.getElementById("fire").addEventListener("click", function () {
+        alert("FIRE DEPARTMENT NUMBER:425-452-6892");
+    });
+
+    document.getElementById("ambulance").addEventListener("click", function () {
+        alert("AMBULANCE NUMBER: 425-452-6892");
+    });
 });
 
-
 //======================================
-// function defintions
+// function definitions
 function createList() {
     // clear prior data
     var myul = document.getElementById("myul");
     myul.innerHTML = "";
-    count = 1;
+      count = 1;   
     contactArray.forEach(function (element) {   // use handy array forEach method
         var li = document.createElement('li');
-          // added data-role="listview" to the ul in the html
-        li.innerHTML = "<li>" + "ID: " + element.ID + "<b> Name: </b>" + element.name + " <b> Email: </b>" + element.email + "<b> Phone Number: </b>" + element.phoneNumber + "</li>";
-        count++;
+        // added data-role="listview" to the ul in the html
+        li.innerHTML = "ID: " + element.ID + "<b> Name: </b>" + element.name + " <b> Email: </b>" + element.email + "<b> Phone Number: </b>" + element.phoneNumber;
+      count++;
         myul.appendChild(li);
     });
 };
